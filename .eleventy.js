56b0f836fe50faba5eaf804f506008366a005b81
decayek <50326711+dmeeco@users.noreply.github.com>
2023-09-28 16:55:07 +0100

    new updated

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,7 @@ async function imageShortcode(src, alt, sizes) {
     widths: [300, 800, null],
     formats: ["avif", "jpeg"],
     urlPath: "/images/",
-    outputDir: "./public/images/"
+    outputDir: "./dist/images/"
   });
 
   let imageAttributes = {
@@ -31,7 +31,7 @@ module.exports = function (eleventyConfig) {
   return {
     dir: {
       input: "src",
-      output: "public"
+      output: "dist"
     }
   };
 };
